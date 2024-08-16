88192e168305b5896e5a2264ec8ad93b693b4729
Zetos Lima <cesar_manoel@hotmail.com>
2024-08-16 16:08:14 -0300

    build: clean pkgs

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
 
   await app.listen(3000);
 }
